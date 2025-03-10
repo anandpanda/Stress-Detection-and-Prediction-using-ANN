e7606da30a541bf892a5d5c971df5bb60e32f42d
Anand Panda <anand612p@gmail.com>
2025-03-11 04:49:25 +0530

    Update run.py to configure port from environment variable and disable debug mode

--- CHIILL_CHECK/run.py
+++ CHIILL_CHECK/run.py
@@ -1,6 +1,8 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=False)  # Disable debug mode in production
