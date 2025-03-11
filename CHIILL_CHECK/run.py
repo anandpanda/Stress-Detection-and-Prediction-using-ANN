d2c40e2a96998829707750412fbb83ef2db38433
Anand Panda <anand612p@gmail.com>
2025-03-11 12:18:27 +0530

    Update run.py to change port to 10000 and add start.sh for Gunicorn server

--- CHIILL_CHECK/run.py
+++ CHIILL_CHECK/run.py
@@ -3,6 +3,5 @@ import os
 
 app = create_app()
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default to 5000
-    app.run(host="0.0.0.0", port=port, debug=False)  # Disable debug mode in production
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
